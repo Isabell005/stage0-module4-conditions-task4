62ef9053400c6046c802a201411b8f03a4d4249e
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-17 12:24:59 +0400

    stage0Module4ConditionsTask4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        first = 5;
+        second = 3;
+        third = 2;
+        String result ="1. Greatest from 3.";
+        String result1="error";
+        System.out.println(first>second?result:result1);
+
+
     }
 }

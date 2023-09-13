768ef50f578ba8dfc1d2283da1cca00acae919fa
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-13 19:25:45 +0400

    stage0Module4ConditionsTask4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -3,7 +3,8 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
 
-        System.out.println(second >= first ? second : first);
+
+        System.out.println((first > second) ? "first" : "second");
     }
 }
 
